17543ecf46b1191c6cd404e8fa576195763b1e94
Marco8313 <marcriviere@rocketmail.com>
2024-08-30 18:18:39 +0200

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -61,6 +61,148 @@ class HomePage extends StatelessWidget {
   }
 }
 
+// Page du calculateur de course
+class RunningCalculatorPage extends StatefulWidget {
+  const RunningCalculatorPage({super.key});
+
+  @override
+  _RunningCalculatorPageState createState() => _RunningCalculatorPageState();
+}
+
+class _RunningCalculatorPageState extends State<RunningCalculatorPage> {
+  final TextEditingController _vitesseController = TextEditingController();
+  final TextEditingController _allureController = TextEditingController();
+  String _resultatConversion = "";
+  double vitesseconvertie = 0;
+
+  void _convertirVitesseEnAllure() {
+    final double vitesse = double.tryParse(_vitesseController.text) ?? 0;
+    if (vitesse > 0) {
+      final double allureDecimale = 60 / vitesse;
+      final int minutes = allureDecimale.floor();
+      final int secondes = ((allureDecimale - minutes) * 60).round();
+
+      setState(() {
+        _resultatConversion = 'Allure: $minutes min $secondes s par km';
+      });
+    } else {
+      setState(() {
+        _resultatConversion = 'Veuillez entrer une vitesse valide.';
+      });
+    }
+  }
+
+  void _convertirAllureEnVitesse() {
+    final double allure = double.tryParse(_allureController.text) ?? 0;
+    if (allure > 0) {
+      final double allureDecimale = allure;
+      final int minutes = allureDecimale.floor();
+      final int secondes = ((allureDecimale - minutes) * 100).round();
+      final int allureensecondes = ((minutes * 60) + secondes);
+      final double vitesse = (3600 / allureensecondes);
+      setState(() {
+        _resultatConversion = 'Vitesse: ${vitesse.toStringAsFixed(1)} km/h\n';
+        vitesseconvertie = vitesse;
+      });
+    }
+  }
+
+  void _calculerPourcentagesvitesse() {
+    double vitesse = vitesseconvertie;
+    if (vitesse < 1) {
+      vitesse = double.tryParse(_vitesseController.text) ?? 0;
+    }
+
+    if (vitesse > 0) {
+      final List<double> percentages = [
+        0.50,
+        0.55,
+        0.60,
+        0.65,
+        0.70,
+        0.75,
+        0.80,
+        0.85,
+        0.90,
+        0.95,
+        1.00,
+        1.05,
+        1.10
+      ];
+      String results = "";
+
+      for (double percentage in percentages) {
+        final double vitessePercentage = vitesse * percentage;
+        final double allureDecimale = 60 / vitessePercentage;
+        final int minutes = allureDecimale.floor();
+        final int secondes = ((allureDecimale - minutes) * 60).round();
+
+        results +=
+            '${(percentage * 100).toStringAsFixed(0)}% Vitesse: ${vitessePercentage.toStringAsFixed(1)} km/h - ${(percentage * 100).toStringAsFixed(0)}% Allure: $minutes min $secondes s par km\n';
+      }
+
+      setState(() {
+        _resultatConversion = results;
+        vitesseconvertie = 0;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculateur de Course'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              controller: _vitesseController,
+              decoration: const InputDecoration(
+                labelText: 'Entrez votre vitesse (km/h)',
+              ),
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _convertirVitesseEnAllure,
+              child: const Text('Convertir Vitesse en Allure'),
+            ),
+            const SizedBox(height: 20),
+            TextField(
+              controller: _allureController,
+              decoration: const InputDecoration(
+                labelText: 'Entrez votre allure (min.km)',
+              ),
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _convertirAllureEnVitesse,
+              child: const Text('Convertir Allure en Vitesse'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _calculerPourcentagesvitesse,
+              child: const Text('Calculer les vitesses selon les pourcentages'),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              _resultatConversion,
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 // Page du calculateur CrossFit
 class CrossFitCalculatorPage extends StatefulWidget {
   const CrossFitCalculatorPage({super.key});
@@ -167,26 +309,6 @@ class _CrossFitCalculatorPageState extends State<CrossFitCalculatorPage> {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-// Page du calculateur de course
-class RunningCalculatorPage extends StatelessWidget {
-  const RunningCalculatorPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Calculateur de Course'),
-      ),
-      body: const Center(
-        child: Text(
-          'Page du calculateur de course.',
-          style: TextStyle(fontSize: 24),
         ),
       ),
     );
